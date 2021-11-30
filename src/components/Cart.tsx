ba635de85f36a6af25b8871a99da443b3484d9da
Luka <luka.dimnik@zervant.com>
2021-11-30 21:24:06 +0200

    Added several component skeletons
    Added drafts of About, Cart, Home, and Products components.

---
+++ src/components/Cart.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+
+const Cart: FC = () => {
+    return <div className="cart-layout">
+        <div>
+            <h1>Your Cart</h1>
+            <p>You have not added any product to your cart yet.</p>
+        </div>
+    </div>;
+}
+
+export default Cart;
---
+++ src/components/Products.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from "react";
+
+const Products: FC = () => {
+    return <div className="products-layout">
+        <h1>Products</h1>
+        <p>Take a look at our products</p>
+    </div>;
+}
+
+export default Products;
