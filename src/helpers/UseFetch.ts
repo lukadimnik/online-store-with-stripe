9bc05b8f4f208fc65353c3d9646e27fedf07b640
Luka <luka.dimnik88@gmail.com>
2021-12-03 14:51:46 +0100

    Added UseFetch custom hook

---
+++ src/helpers/UseFetch.ts
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+
+const useFetch = (baseUrl: string) => {
+  const [loading, setLoading] = useState(true);
+
+  const get = (url: string) => {
+    return new Promise((resolve, reject) => {
+      fetch(baseUrl + url)
+        .then((response) => response.json())
+        .then((data) => {
+          if (!data) {
+            setLoading(false);
+            return reject(data);
+          }
+          setLoading(false);
+          resolve(data);
+        })
+        .catch((error) => {
+          setLoading(false);
+          reject(error);
+        });
+    });
+  };
+
+  const post = (url: string, body) => {
+    return new Promise((resolve, reject) => {
+      fetch(baseUrl + url, {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          if (!data) {
+            setLoading(false);
+            return reject(data);
+          }
+          setLoading(false);
+          resolve(data);
+        })
+        .catch((error) => {
+          setLoading(false);
+          reject(error);
+        });
+    });
+  };
+
+  return { get, post, loading };
+};
+
+export default useFetch;
